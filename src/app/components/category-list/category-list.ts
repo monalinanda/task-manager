d5f479d93290540fbee4e8a4e0a21ca5708f07bd
monalinanda <monalinanda95@gmial.com>
2025-06-08 17:03:20 +0300

    add category and task html and css files

--- src/app/components/category-list/category-list.ts
+++ src/app/components/category-list/category-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './category-list.scss'
 })
 export class CategoryList {
-
+ constructor(
+  private router: Router
+ ){}
+ 
+ navigateToCreateCategory() {
+ this.router.navigate(['/categories/new']);
+ }
 }
